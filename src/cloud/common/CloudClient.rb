b4bad34d0c7289e0603762a2c412e03a66fe7ec3
Ruben S. Montero <rsmontero@opennebula.org>
2019-04-11 14:44:51 +0200

    Revert "Bump version to 5.6.3 (#2499)"
    This reverts commit f11c050e269241b7d147fcd91c0d73088d9ebb02.

--- src/cloud/common/CloudClient.rb
+++ src/cloud/common/CloudClient.rb
@@ -50,7 +50,7 @@ end
 module CloudClient
 
     # OpenNebula version
-    VERSION = '5.6.3'
+    VERSION = '5.6.2'
 
     # #########################################################################
     # Default location for the authentication file
