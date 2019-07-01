f94f88b9261bc99bfc8e4b8b81cc974a61f37c25
Christian González <cgonzalez@opennebula.systems>
2019-07-01 11:58:06 +0200

    Make HM_MAD configurable

--- src/hm_mad/one_hm.rb
+++ src/hm_mad/one_hm.rb
@@ -29,6 +29,7 @@ end
 $LOAD_PATH << RUBY_LIB_LOCATION
 
 require 'OpenNebulaDriver'
+require 'getoptlong'
 require 'rubygems'
 require 'ffi-rzmq'
 
@@ -45,13 +46,20 @@ class HookManagerDriver < OpenNebulaDriver
         super('', @options)
 
         # Initialize publisher socket
-        context = ZMQ::Context.new(1)
-        @publisher = context.socket(ZMQ::PUB)
+        context_pub = ZMQ::Context.new(1)
+        @publisher = context_pub.socket(ZMQ::PUB)
+        @publisher.setsockopt(ZMQ::SNDHWM, @options[:hwm]) unless @options[:hwm].nil?
+
+        context_rep = ZMQ::Context.new(1)
+        @replier = context_rep.socket(ZMQ::REP)
 
         # TODO, make the port configurable and add HWM option
-        @publisher.bind('tcp://*:5556')
+        @publisher.bind("tcp://*:#{@options[:publisher_port]}")
+        @replier.bind("tcp://*:#{@options[:logger_port]}")
 
         register_action(:EXECUTE, method('action_execute'))
+
+        # TODO, new thread
     end
 
     def action_execute(type, *arguments)
@@ -59,12 +67,48 @@ class HookManagerDriver < OpenNebulaDriver
         key = "#{type} #{arguments.shift(2).join(' ')}"
         vals = arguments.join(' ')
 
-        # using envelopes for splitting key/val (http://zguide.zeromq.org/page:all#Pub-Sub-Message-Envelopes)
+        # Using envelopes for splitting key/val (http://zguide.zeromq.org/page:all#Pub-Sub-Message-Envelopes)
         @publisher.send_string key, ZMQ::SNDMORE
         @publisher.send_string vals
     end
 
 end
 
-hm = HookManagerDriver.new(:concurrency => 15)
+# Hook Manager main program
+
+opts = GetoptLong.new(
+    ['--threads',        '-t', GetoptLong::OPTIONAL_ARGUMENT],
+    ['--publisher-port', '-p', GetoptLong::OPTIONAL_ARGUMENT],
+    ['--logger-port',    '-l', GetoptLong::OPTIONAL_ARGUMENT],
+    ['--hwm',            '-h', GetoptLong::OPTIONAL_ARGUMENT]
+)
+
+threads        = 15
+publisher_port = 5556
+logger_port    = 5555
+hwm            = nil # http://zguide.zeromq.org/page:all#High-Water-Marks
+
+begin
+    opts.each do |opt, arg|
+        require 'pry'
+        case opt
+        when '--threads'
+            threads = arg.to_i
+        when '--publisher-port'
+            publisher_port = arg.to_i
+        when '--logger-port'
+            logger_port = arg.to_i
+        when '--hwm'
+            hwm = arg.to_i
+        end
+    end
+rescue Exception => e
+    exit(-1)
+end
+
+hm = HookManagerDriver.new(:concurrency => threads,
+                           :publisher_port => publisher_port,
+                           :logger_port => logger_port,
+                           :hwm => hwm)
+
 hm.start_driver
