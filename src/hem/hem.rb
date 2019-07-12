0f2899d1cadc113953edf65e70c14e888a564cbb
Christian González <cgonzalez@opennebula.systems>
2019-07-12 17:41:31 +0200

    Add hem

---
+++ src/hem/hem.rb
@@ -0,0 +1,286 @@
+# -------------------------------------------------------------------------- #
+# Copyright 2002-2019, OpenNebula Project, OpenNebula Systems                #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License. You may obtain    #
+# a copy of the License at                                                   #
+#                                                                            #
+# http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+#--------------------------------------------------------------------------- #
+
+require 'rubygems'
+require 'ffi-rzmq'
+require 'nokogiri'
+require 'yaml'
+require 'log'
+require 'logger'
+require 'opennebula'
+
+ONE_LOCATION = ENV['ONE_LOCATION']
+
+if !ONE_LOCATION
+    LOG_LOCATION = '/var/log/one'
+    VAR_LOCATION = '/var/lib/one'
+    ETC_LOCATION = '/etc/one'
+    LIB_LOCATION = '/usr/lib/one'
+    RUBY_LIB_LOCATION = '/usr/lib/one/ruby'
+else
+    VAR_LOCATION = ONE_LOCATION + '/var'
+    LOG_LOCATION = ONE_LOCATION + '/var'
+    ETC_LOCATION = ONE_LOCATION + '/etc'
+    LIB_LOCATION = ONE_LOCATION + '/lib'
+    RUBY_LIB_LOCATION = ONE_LOCATION + '/lib/ruby'
+end
+
+CONFIGURATION_FILE = ETC_LOCATION + '/hem.conf'
+HEM_LOG            = LOG_LOCATION + '/hem.log'
+
+##############################################################################
+# Constants
+##############################################################################
+
+# List of different hook types
+HOOK_TYPES = [:API, :STATE]
+
+# List of API calls which trigger hooks reload
+STATIC_FILTERS = [
+    'API one.hook.update 1',
+    'API one.hook.allocate 1',
+    'API one.hook.delete 1'
+]
+
+# Mon Feb 27 06:02:30 2012 [Clo] [E]: Error message example
+MSG_FORMAT  = %(%s [%s]: %s\n)
+
+# Mon Feb 27 06:02:30 2012
+DATE_FORMAT = '%a %b %d %H:%M:%S %Y'
+
+##############################################################################
+# Configuration
+##############################################################################
+
+begin
+    conf = YAML.load_file(CONFIGURATION_FILE)
+rescue Exception => e
+    STDERR.puts "Error parsing config file #{CONFIGURATION_FILE}: #{e.message}"
+    exit 1
+end
+
+conf[:hook_base_path] ||= '/var/lib/one/remotes/hook'
+conf[:subscriber_endpoint] ||= 'tcp://localhost:5556'
+conf[:replier_endpoint] ||= 'tcp://localhost:5557'
+conf[:debug_level] ||= 2
+
+##############################################################################
+# Log
+##############################################################################
+
+DEBUG_LEVEL = [
+    Logger::ERROR, # 0
+    Logger::WARN,  # 1
+    Logger::INFO,  # 2
+    Logger::DEBUG  # 3
+]
+
+logger = Logger.new(HEM_LOG)
+logger.level = DEBUG_LEVEL[conf[:debug_level].to_i]
+logger.formatter = proc do |severity, datetime, _progname, msg|
+    format(MSG_FORMAT,
+           datetime.strftime(DATE_FORMAT),
+           severity[0..0],
+           msg)
+end
+
+##############################################################################
+# Helpers
+##############################################################################
+
+# Generates a key for a given hook
+def get_key(hook)
+    type = hook['TYPE'].to_sym
+
+    return hook['TEMPLATE/CALL'] if type == :API
+end
+
+# Load Hooks from oned (one.hookpool.info)
+def load_hooks()
+    client = nil
+
+    if !conf[:auth].nil?
+        client = Client.new(conf[:auth], conf[:one_xmlrpc])
+    else
+        client = Client.new(nil, conf[:one_xmlrpc])
+    end
+
+    logger.info('Loading Hooks...')
+
+    # TODO, manage errors
+    hook_pool = HookPool.new(client)
+    hook_pool.info
+
+    hooks = {}
+    HOOK_TYPES.each do |type|
+        hooks[type] = {}
+    end
+
+    hook_pool.each do |hook|
+        type = hook['TYPE']
+
+        if HOOK_TYPES.include? type.to_sym
+            logger.error("Error loading hooks. Invalid hook type: #{type}")
+            next
+        end
+
+        hooks[type.to_sym][get_key(hook)] = {
+            :id => hook['ID'],
+            :name => hook['NAME'],
+            :command => hook['TEMPLATE/COMMAND'],
+            :remote => hook['TEMPLATE/REMOTE'],
+            :args => hook['TEMPLATE/ARGS']
+        }
+    end
+
+    logger.info('Hooks successfully loaded')
+
+    hooks
+end
+
+# Generate a filter for an API type hook
+def gen_filter_api(key, _hook)
+    # TODO, subscribed for success, fail, always
+    "#{type} #{key} 1"
+end
+
+# Generate a filter for a STATE type hook
+def gen_filter_state(_key, _hook)
+    # TODO
+    ''
+end
+
+# Generate a filter for a Hook given the typw and the key
+def gen_filter(type, key, hook)
+    return gen_filter_api(key, hook)   if type == :API
+    return gen_filter_state(key, hook) if type == :STATE
+end
+
+# Subscribe the given socket to the given filter
+# The socket must be of type SUBSCRIBER
+def subscribe(socket, filter)
+    socket.setsockopt(ZMQ::SUBSCRIBE, filter)
+end
+
+# Unsubscribe the given socket from the given filter
+# The socket must be of type SUBSCRIBER
+def unsubscribe(socket, filter)
+    socket.setsockopt(ZMQ::UNSUBSCRIBE, filter)
+end
+
+# Subscribe the socket to all the Hooks from hook pool and to the STATIC_FILTERS
+def setup_filters(socket, hooks)
+    # Subscribe to hooks modifier calls
+    STATIC_FILTERS.each do |filter|
+        subscribe(socket, filter)
+    end
+
+    # Subscribe to each existing hook
+    HOOK_TYPES.each do |type|
+        hooks[type].each do |hook|
+            subscribe(socket, gen_filter(type, hook[0], hook[1]))
+        end
+    end
+end
+
+# Parse hook arguments
+def parse_args(args, key, content)
+    params = ''
+    alloc = key[1].include? 'allocate'
+
+    if args.nil?
+        return params
+    end
+
+    args = args.split ' '
+    args.each do |arg|
+        if arg == '$API'
+            if alloc
+                params << ' ' << content.split(' ')[0..-2].join(' ')
+            else
+                params << ' ' << content
+            end
+
+            next
+        elsif arg == '$ID' && alloc
+            params << ' ' << content.split(' ')[-1]
+
+            next
+        elsif arg == '$TEMPLATE'
+            # TODO, get template (API call, auth?)
+            next
+        end
+    end
+
+    params
+end
+
+##############################################################################
+# Main
+##############################################################################
+
+# Zeromq context
+context = ZMQ::Context.new(1)
+
+# Subscriber socket for receiving the events
+subscriber = context.socket(ZMQ::SUB)
+subscriber.connect(conf[:subscriber_endpoint])
+
+# Requester socket for returning back the execution result
+requester = context.socket(ZMQ::REQ)
+requester.connect(conf[:replier_endpoint])
+
+# Load hooks from oned
+hooks = load_hooks
+
+# Set up subscriber filters
+setup_filters(subscriber, hooks)
+
+loop do
+    key = ''
+    content = ''
+    ack = ''
+
+    subscriber.recv_string(key)
+    subscriber.recv_string(content)
+
+    key = key.split(' ')
+
+    hook_info = hooks[key[0].upcase.to_sym][key[1]]
+
+    if UPDATE_CALLS.include? key[1]
+        # TODO, reload hooks
+        puts 'Reloading hooks'
+    end
+
+    next if hook_info.nil?
+
+    # trigger hook (get params, execute, return exec result)
+    params = parse_args(hook_info[:args], key, content)
+    rc = system("#{HOOKS_BASE_PATH}/#{hook_info[:command]} #{params}")
+
+    if rc
+        rc = '1'
+    else
+        rc = '0'
+    end
+
+    requester.send_string("#{rc} '#{hook_info[:name]}' #{hook_info[:id]} #{params}")
+
+    requester.recv_string(ack)
+
+    exit(-1) if ack != 'ACK'
+end
